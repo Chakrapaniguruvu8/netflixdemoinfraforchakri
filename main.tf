33e5b4ab1f7861de6711602c73346a02e9e28eff
Chakrapani guruvu <163977881+Chakrapaniguruvu8@users.noreply.github.com>
2025-09-04 18:00:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,27 @@
 provider "aws" {
-region = "ap-south-1"
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "one" {
-count = 5
-ami = "ami-0f918f7e67a3323f0"
-instance_type = "t2.medium"
-key_name = "RMUMBAIKP"
-vpc_security_group_ids = ["sg-0da91ed9997d755fc"]
-tags = {
-Name = var.instance_names[count.index]
-}
+  count         = 4
+  ami           = "ami-02d26659fd82cf299"
+  instance_type = "t3.medium"
+  key_name      = "chkpair"
+
+  vpc_security_group_ids = [
+    "sg-070eb542e17ea82af"
+  ]
+
+  tags = {
+    Name = var.instance_names[count.index]
+  }
 }
 
 variable "instance_names" {
-default = ["jenkins", "nexus", "AppServer-1", "AppServer-2", "Monitoring server"]
+  default = [
+    "jenkins",
+    "AppServer-1",
+    "AppServer-2",
+    "Monitoring server"
+  ]
 }
